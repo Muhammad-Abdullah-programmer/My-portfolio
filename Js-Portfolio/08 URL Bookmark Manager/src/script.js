80392736fcd2c55a11ca1de9c5c9726d2c2616b1
MuhammadAbdullah <business.mabdullah@gmail.com>
2025-10-15 09:28:27 +0500

    project of URL Bookmark Manager continue html part complete js element selecting part complete but not completed yet working on it

---
+++ Js-Portfolio/08 URL Bookmark Manager/src/script.js
@@ -0,0 +1,7 @@
+// Select Elements 
+const form = document.getElementById("bookmark-form")
+const nameInput = document.getElementById("name")
+const urlInput = document.getElementById("url")
+const tagsInput = document.getElementById("tags")
+const bookmarkList = document.getElementById("bookmark-list")
+const searchInput = document.getElementById("search")
