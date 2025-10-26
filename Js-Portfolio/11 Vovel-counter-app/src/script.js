2311a723ed1e5940c60bd1c2c2d2d9f1512a3c22
Muhammad-Abdullah-programmer <business.mabdullah@gmail.com>
2025-10-26 04:13:32 -0700

    vowel counter project added in js portfolio

---
+++ Js-Portfolio/11 Vovel-counter-app/src/script.js
@@ -0,0 +1,24 @@
+const input = document.getElementById("textInput");
+const btn = document.getElementById("countBtn");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", () => {
+  const text = input.value.toLowerCase();
+  let count = 0;
+
+  for (let char of text) {
+    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+      count++;
+    }
+  }
+
+  if (text.length === 0) {
+    message.textContent = "Please type something first!";
+    message.style.color = "yellow";
+  } else {
+    message.textContent = `There are ${count} vowels in your text.`;
+    message.style.color = "lightgreen";
+  }
+
+  input.value = ""
+});
