465b66ca78ea1b93ad115a4bf310d0207cec3071
Muhammad-Abdullah-programmer <business.mabdullah@gmail.com>
2026-04-15 10:17:44 +0500

    1st product in progress of product section

--- Js-Portfolio/15 E-commerece/src/Js/tShirtProducts.js
+++ Js-Portfolio/15 E-commerece/src/Js/tShirtProducts.js
@@ -10,14 +10,21 @@ let tshirtProducts = document.getElementById("tShirt-all-p")
 
 tshirtProducts.innerHTML = tShirts.map((allProducts)=>{
 
-   return `<div class="p-tshirt-1 col-span-1 aspect-square bg-blue-700"> 
+   return `<div class="p-tshirt-1 col-span-1 aspect-square bg-[#f4f4f6] flex flex-col relative group"> 
     
   
-    <img src="${allProducts.url}"
+    <img src="${allProducts.url}">
 
+   
+  
     <div class="flex flex-col gap-2">
       <h2 class="text-lg font-normal"> ${allProducts.title}</h2>
     <span class="text-[#f85606] text-2xl font-bold"> ${allProducts.price}</span>
     </div>
+
+    <div  class=" bottom-16 left-2 px-2 py-1 shadow transform translate-y-5 opacity-0 group-hover:opacity-100 group-hover:-translate-y-1 transition ease-in-out duration-300 bg-white h-[10%] absolute">
+      <p> Size </p>
+    </div>
+
     </div>`
 }).join("")
