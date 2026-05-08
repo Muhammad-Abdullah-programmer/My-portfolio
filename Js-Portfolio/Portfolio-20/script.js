f4a61d53ded69ea4904fb6632ce842688a9628ec
Muhammad-Abdullah-programmer <business.mabdullah@gmail.com>
2026-05-08 10:05:12 +0500

    uploaded some projects

--- Js-Portfolio/Portfolio-20/script.js
+++ Js-Portfolio/Portfolio-20/script.js
@@ -2,6 +2,8 @@ let submitBtn = document.getElementById("submit")
 let nameInpt = document.getElementById("nameInpt")
 let numInpt = document.getElementById("numInpt")
 let mailInpt = document.getElementById("mailInpt")
+let dark = document.getElementById("dark")
+let light = document.getElementById("light")
 
 
 submitBtn.addEventListener("click",()=>{
@@ -10,7 +12,28 @@ submitBtn.addEventListener("click",()=>{
 
   if(cleenValue === ""){
     alert("please write something")
-  }else if(!/^[A-Za-z]+$/.test(cleenValue)){
+  }else if(!/^[A-Za-z ]+$/.test(cleenValue)){
     alert("Please write valid name Text only")
+  }else{
+    alert("Thanks for connecting us")
   }
 })
+
+
+
+
+
+const toggleBtn = document.getElementById("themeToggle");
+const html = document.documentElement;
+
+if (localStorage.theme === "dark") {
+  html.classList.add("dark");
+}
+
+toggleBtn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+
+  localStorage.theme = html.classList.contains("dark")
+    ? "dark"
+    : "light";
+});
